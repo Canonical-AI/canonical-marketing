a726b86192d664e39b31eae6a43fc69fd7ecf73e
john <johnjAzzinaro@gmail.com>
2024-10-23 14:40:41 -0400

    more updates

--- src/App.vue
+++ src/App.vue
@@ -11,16 +11,20 @@
         </div>
         <div class="space-x-4">
           <a class="text-on-surface-variant hover:text-primary transition-colors" @click="toggleFAQ">About</a>
-          <a href="https://canonical-dev-b6afd.firebaseapp.com" class="bg-warning text-white px-4 py-2 rounded-md hover:bg-primary-darken-1 transition-colors">try</a>
+          <a href="https://canonical-dev-b6afd.firebaseapp.com/document/7Smjq3YGDK2YW2ULrbMv?v=v1.0.0" class="bg-warning text-white px-4 py-2 rounded-md hover:bg-primary-darken-1 transition-colors">try</a>
         </div>
       </nav>
     </header>
 
-    <div v-if="openFAQ" class="faq p-8 px-24 bg-surface z-50">
+    <div v-if="openFAQ" class="faq p-8 px-24 bg-surface z-50 overflow-auto">
       <h1 class="text-left text-2xl mb-5 font-light">Frequently Asked Questions</h1>
       <div v-for="(faq, index) in faqs" :key="index" class="faq-item text-left mb-5">
         <h2 class="text-lg mb-2.5 font-semibold">{{ faq.question }}</h2>
         <p class="text-md">{{ faq.answer }}</p>
+      </div>
+      <div class="flex justify-end">
+        <a href="https://github.com/your-github-username" class="text-on-surface-variant hover:text-primary transition-colors mx-2">GitHub</a>
+        <a href="https://www.linkedin.com/in/your-linkedin-username" class="text-on-surface-variant hover:text-primary transition-colors mx-2">LinkedIn</a>
       </div>
     </div>
 
@@ -59,8 +63,8 @@ const toggleFAQ = () => {
 const faqs = ref([
 
 { question: 'What is Canonical?', answer: 'Canonical is a platform for artifact creation, curation, and collaboration. Designed for product people by a proudct person. Why should devs get all the cool tools?. My vision is to blur the lines between engineering and product work and enable more people to build great products.' },
-{ question: 'Who Built Canonical?', answer: 'My name is John Azzinaro, Ex-AWS Technical Product Manager, with over five years of product management experience, and 10 years working in engineering.' },
-{ question: 'Why Did you build it?', answer: 'Ive been coding for over a decade, and I wanted to build a tool that would make it easier for the people doing all the "soft stuff" to do their jobs like engineers do. in engineering we have concepts like commits, versioning, referencing and reviewing. This is the first step in that direction.' },
+{ question: 'Who built Canonical?', answer: 'My name is John Azzinaro, Ex-AWS Technical Product Manager, with over five years of product management experience, and 10 years working in engineering.' },
+{ question: 'Why did you build Canonical?', answer: 'Ive been coding for over a decade, and I wanted to build a tool that would make it easier for the people doing all the "soft stuff" to do their jobs like how engineers do their coding. in engineering we have concepts like commits, versioning, referencing and reviewing. This is the first step in that direction.' },
 { question: 'Whats next?', answer: 'Visit Github to see the public roadmap, or visit the demo site to see all of the product artifacts for Canonical live!' },
 // Add more FAQs as needed
 ]);
